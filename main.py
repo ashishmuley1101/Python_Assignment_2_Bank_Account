f5c5c371a2a3fd0da2b540478526a44f6c2a0273
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-15 16:55:23 +0530

    UC 7. The complete executed code for the  BankAccount class.

--- main.py
+++ main.py
@@ -51,6 +51,42 @@ class BankAccount:
           print("Account Balance : ", self.balance, "Rs.")
 
 
+# UC 7. The complete executed code for the  BankAccount class.
+flag = 0
+while (flag == 0):
+     print("Please choose the option for Account Manage : \n1. Create new Account.\t2.Withdrawal\t3. Deposit\t4. Account Details\t5. Exit   ")
+
+     option = int(input("Enter your option : "))
+
+     if option == 1:
+          accountNumber = int(input("Enter the Account Number : "))
+          name = input("Enter the Account holder Name : ")
+          balance = int(input("Enter Account opening Balance : "))
+
+          # Initialized the Constructor with parameters: accountNumber, name and balance.
+          bankAccount = BankAccount(accountNumber, name, balance)
+
+     elif option == 2:
+          # Calling the withdrawal() for account manage action.
+          withdrawal = int(input("Enter the amount to withdrawal : "))
+          bankAccount.Withdrawal(withdrawal)
+
+     elif option == 3:
+          # Calling the Deposit() for account manage action.
+          deposit = int(input("Enter the amount to deposit : "))
+          bankAccount.Deposit(deposit)
+
+     elif option == 4:
+          # Calling the display() to display the account details.
+          bankAccount.display()
+
+     elif option == 5:
+          flag += 1
+
+     else:
+          print("Invalid option...! ")
+
+
 
 
 
